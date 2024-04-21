b2854571ca2a4ee45d397ea297300f3b2f376a95
BetaDevv <miguelangel.bet0211@gmail.com>
2024-04-21 03:33:59 +0000

    Mensaje descriptivo de los cambios

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xv_jbu4$!$$*&1j1o9@13www*@#fns%8zhp_0teut%v+u*mxjs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['BetaDevv.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'services.apps.ServicesConfig',
     'social.apps.SocialConfig',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'webempresa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
