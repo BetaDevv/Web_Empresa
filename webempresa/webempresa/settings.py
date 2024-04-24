e6aef568ebfa437dfd4eabe9f919fe74427c3775
BetaDevv <miguelangel.bet0211@gmail.com>
2024-04-24 15:20:30 +0000

    actualizacion de staticos

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xv_jbu4$!$$*&1j1o9@13www*@#fns%8zhp_0teut%v+u*mxjs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['BetaDevv.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['betadevv.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'webempresa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 #Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
